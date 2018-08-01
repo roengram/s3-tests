28f9d1cf305c33471de261ba3aa23d9fcc227184
Jaehoon Ko <roengram@gmail.com>
2018-08-01 10:18:40 +0900

    fix test script

--- run-minio-svr.sh
+++ run-minio-svr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-S3TEST_CONF=../config/config.minio.svr ../virtualenv/bin/nosetests \
+S3TEST_CONF=config/config.minio.svr virtualenv/bin/nosetests \
             -e delimiter \
             -e fuzzer \
             -e website \
@@ -9,6 +9,6 @@ S3TEST_CONF=../config/config.minio.svr ../virtualenv/bin/nosetests \
             -e acl \
             -a '!bucket-policy,!fails_on_aws,!fails_with_subdomain,!lifecycle,!tagging,!ACLs' \
             --processes=0 \
-            ../s3tests/functional/test_s3.py \
+            s3tests/functional/test_s3.py \
             &> res/minio.svr &
 #            -v  --collect-only \
